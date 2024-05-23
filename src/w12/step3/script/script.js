a17535aeef734131cb5304ca785831160ec60025
spctrm404 <46218112+spctrm404@users.noreply.github.com>
2024-05-23 12:01:37 +0900

    end of w12

---
+++ src/w12/step3/script/script.js
@@ -0,0 +1,16 @@
+let elemBody = document.body;
+elemBody.style.backgroundColor = '#0000ff';
+// let aCard = document.querySelector('.card');
+// aCard.addEventListener('click', function () {
+//   console.log('클릭!');
+//   aCard.classList.toggle('card--state-clicked');
+//   console.log(aCard.classList);
+// });
+let cards = document.querySelectorAll('.card');
+console.log(cards);
+for (let i = 0; i < cards.length; i++) {
+  let eachCard = cards[i];
+  eachCard.addEventListener('click', function () {
+    eachCard.classList.toggle('card--state-clicked');
+  });
+}
